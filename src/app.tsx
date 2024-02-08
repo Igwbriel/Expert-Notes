d3f82a27afe0fa27ae59f290754f3a7fac1273a2
Gabriel Dias <GabrielDias7820@gmail.com>
2024-02-08 17:56:05 -0300

    Exibindo novas notas dinamicamente

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,26 @@
+import { useState } from 'react'
 import logo from './assets/Logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { Notecard } from './components/note-card'
 
+interface Note {
+  id: string
+  date: Date
+  content: string
+}
+
 export function App() {
+  const [notes, setNotes] = useState<Note[]>([])
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+    setNotes([newNote, ...notes])
+  }
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW expert" />
@@ -18,14 +36,11 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
 
-        <Notecard
-          note={{
-            date: new Date(),
-            content: 'Hello world',
-          }}
-        />
+        {notes.map((note) => {
+          return <Notecard key={note.id} note={note} />
+        })}
       </div>
     </div>
   )
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,7 +3,11 @@ import { X } from 'lucide-react'
 import { ChangeEvent, FormEvent, useState } from 'react'
 import { toast } from 'sonner'
 
-export function NewNoteCard() {
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void
+}
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnbording, setShouldShowOnbording] = useState(true)
   const [content, setContent] = useState('')
 
@@ -21,7 +25,11 @@ export function NewNoteCard() {
 
   function handleSaveNote(event: FormEvent) {
     event.preventDefault()
-    console.log(content)
+
+    onNoteCreated(content)
+
+    setContent('')
+    setShouldShowOnbording(true)
 
     toast.success('Nota criada com sucesso')
   }
@@ -69,6 +77,7 @@ export function NewNoteCard() {
                   autoFocus
                   className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
                   onChange={handleContentChange}
+                  value={content}
                 />
               )}
             </div>
